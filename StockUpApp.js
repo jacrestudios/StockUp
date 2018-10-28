9be2b849885a0a287ece1f5c195ef60fae075a61
jacrestudios <jacrestudios@gmail.com>
2018-10-28 14:25:39 -0600

    attributes added

--- StockUpApp.js
+++ StockUpApp.js
@@ -1,1 +1,30 @@
+(() => {
+	IO : {
+		init: () => {}
+	}
+	App : {
+		role: '',
+		Host: {
+			player_count: 0,
+			transactions: [],
+			cards: [],
+			stocks: [],
+			player_statuses: [],
+			game_start: false,
+			turn: 0,
+		},
+		Player: {
+			role: '',
+			name: '',
+			networth: '',
+			stocks: [],
+			cards: [],
+			ready: false,
+		},
+		Display: {
+			
+		},
+	}
+	IO.init();
+})();
 
