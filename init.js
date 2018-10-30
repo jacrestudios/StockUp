4e0fbb66d1341a8b7faffda1f643a39d8f267822
Jacre <jacrestudios@gmail.com>
2018-10-30 18:21:21 +0000

    Populated init.js, shouldn't need to be updated

--- init.js
+++ init.js
@@ -1,1 +1,20 @@
 
+var express = require('express');
+var path = require('path');
+var app = express();
+var game = require('./StockUpServer');
+
+app.configure(function() {
+    app.use(express.logger('dev'));
+    app.use(express.static(path.join(__dirname,'public')));
+});
+
+var server = require('http').createServer(app).listen(process.env.PORT || 8080);
+
+var io = require('socket.io').listen(server);
+
+io.set('log level',1);
+
+io.sockets.on('connection', socket => {
+    game.init(io, socket);
+});
