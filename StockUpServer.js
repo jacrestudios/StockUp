56d5a9eadd66af02bbfc3801718b7c3341de391f
Jacre <jacrestudios@gmail.com>
2018-11-02 16:44:35 +0000

    Started StockUpServer.js

--- StockUpServer.js
+++ StockUpServer.js
@@ -1,1 +1,13 @@
+var io;
+var rsocket;
 
+exports.init = (sio, socket) => {
+    io = sio;
+    rsocket = socket;
+    rsocket.emit('connected')
+    
+    //Events
+    //Common
+    //Host
+    //Player
+}
